708f3cf43b2dd284894a63154c00600012316696
ChenHu chenhu1008@me.com <chenhu1008@me.com>
2020-08-02 10:42:14 +0800

    格式化代码

--- sky-root/signal-pre/src/main/java/com/sky/signal/pre/processor/odAnalyze/ODSchemaProvider.java
+++ sky-root/signal-pre/src/main/java/com/sky/signal/pre/processor/odAnalyze/ODSchemaProvider.java
@@ -12,110 +12,75 @@ import org.apache.spark.sql.types.StructType;
 public class ODSchemaProvider {
     //带人口分类的枢纽人口轨迹
     public static final StructType STATION_TRACE_CLASSIC_SCHEMA = DataTypes
+            .createStructType(Lists.newArrayList(DataTypes.createStructField("date", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("msisdn", DataTypes.StringType, false),
+                    DataTypes.createStructField("base", DataTypes.StringType, false),
+                    DataTypes.createStructField("lng", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("lat", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("begin_time", DataTypes.TimestampType, false),
+                    DataTypes.createStructField("last_time", DataTypes.TimestampType, false),
+                    DataTypes.createStructField("distance", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("move_time", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("speed", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("point_type", DataTypes.ByteType, false),
+                    DataTypes.createStructField("station_person_classic", DataTypes.ByteType, false)));
+    //枢纽站轨迹
+    public static final StructType STATION_TRACE_SCHEMA = DataTypes
+            .createStructType(Lists.newArrayList(DataTypes.createStructField("date", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("msisdn", DataTypes.StringType, false),
+                    DataTypes.createStructField("base", DataTypes.StringType, false),
+                    DataTypes.createStructField("lng", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("lat", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("begin_time", DataTypes.TimestampType, false),
+                    DataTypes.createStructField("last_time", DataTypes.TimestampType, false),
+                    DataTypes.createStructField("distance", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("move_time", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("speed", DataTypes.DoubleType, false)));
+    //带停留点类型的轨迹
+    public static final StructType TRACE_SCHEMA = DataTypes.createStructType
+            (Lists.newArrayList(DataTypes.createStructField("date", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("msisdn", DataTypes.StringType, false),
+                    DataTypes.createStructField("base", DataTypes.StringType, false),
+                    DataTypes.createStructField("lng", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("lat", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("begin_time", DataTypes.TimestampType, false),
+                    DataTypes.createStructField("last_time", DataTypes.TimestampType, false),
+                    DataTypes.createStructField("distance", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("move_time", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("speed", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("point_type", DataTypes.ByteType, false)));
+
+    public static final StructType OD_TRACE_SCHEMA = DataTypes
+            .createStructType(Lists.newArrayList(DataTypes.createStructField("date", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("msisdn", DataTypes.StringType, false),
+                    DataTypes.createStructField("leave_base", DataTypes.StringType, false),
+                    DataTypes.createStructField("leave_lng", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("leave_lat", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("arrive_base", DataTypes.StringType, false),
+                    DataTypes.createStructField("arrive_lng", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("arrive_lat", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("leave_time", DataTypes.TimestampType, false),
+                    DataTypes.createStructField("arrive_time", DataTypes.TimestampType, false)));
+    public static final StructType OD_SCHEMA = DataTypes.createStructType
+            (Lists.newArrayList(DataTypes.createStructField("date", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("msisdn", DataTypes.StringType, false),
+                    DataTypes.createStructField("leave_base", DataTypes.StringType, false),
+                    DataTypes.createStructField("leave_lng", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("leave_lat", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("arrive_base", DataTypes.StringType, false),
+                    DataTypes.createStructField("arrive_lng", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("arrive_lat", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("leave_time", DataTypes.TimestampType, false),
+                    DataTypes.createStructField("arrive_time", DataTypes.TimestampType, false),
+                    DataTypes.createStructField("linked_distance", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("max_speed", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("cov_speed", DataTypes.DoubleType, false),
+                    DataTypes.createStructField("distance", DataTypes.IntegerType, false),
+                    DataTypes.createStructField("move_time", DataTypes.IntegerType, false)));
+    public static final StructType OD_TRIP_STAT_SCHEMA = DataTypes
             .createStructType(Lists.newArrayList(DataTypes.createStructField
                             ("date", DataTypes.IntegerType, false), DataTypes
                             .createStructField("msisdn", DataTypes.StringType, false)
-                    , DataTypes.createStructField("base", DataTypes
-                            .StringType, false), DataTypes.createStructField
-                            ("lng", DataTypes.DoubleType, false), DataTypes
-                            .createStructField("lat", DataTypes.DoubleType,
-                                    false), DataTypes.createStructField
-                            ("begin_time", DataTypes.TimestampType, false),
-                    DataTypes.createStructField("last_time", DataTypes
-                            .TimestampType, false), DataTypes
-                            .createStructField("distance", DataTypes
-                                    .IntegerType, false), DataTypes
-                            .createStructField("move_time", DataTypes
-                                    .IntegerType, false), DataTypes
-                            .createStructField("speed", DataTypes.DoubleType,
-                                    false), DataTypes.createStructField
-                            ("point_type", DataTypes.ByteType, false),
-                    DataTypes.createStructField("station_person_classic",
-                            DataTypes.ByteType, false)));
-    //枢纽站轨迹
-    public static final StructType STATION_TRACE_SCHEMA = DataTypes
-            .createStructType(Lists.newArrayList(DataTypes.createStructField
-                    ("date", DataTypes.IntegerType, false), DataTypes
-                    .createStructField("msisdn", DataTypes.StringType, false)
-                    , DataTypes.createStructField("base", DataTypes
-                            .StringType, false), DataTypes.createStructField
-                            ("lng", DataTypes.DoubleType, false), DataTypes
-                            .createStructField("lat", DataTypes.DoubleType,
-                                    false), DataTypes.createStructField
-                            ("begin_time", DataTypes.TimestampType, false),
-                    DataTypes.createStructField("last_time", DataTypes
-                            .TimestampType, false), DataTypes
-                            .createStructField("distance", DataTypes
-                                    .IntegerType, false), DataTypes
-                            .createStructField("move_time", DataTypes
-                                    .IntegerType, false), DataTypes
-                            .createStructField("speed", DataTypes.DoubleType,
-                                    false)));
-    //带停留点类型的轨迹
-    public static final StructType TRACE_SCHEMA = DataTypes.createStructType
-            (Lists.newArrayList(DataTypes.createStructField("date", DataTypes
-                    .IntegerType, false), DataTypes.createStructField
-                    ("msisdn", DataTypes.StringType, false), DataTypes
-                    .createStructField("base", DataTypes.StringType, false),
-                    DataTypes.createStructField("lng", DataTypes.DoubleType,
-                            false), DataTypes.createStructField("lat",
-                            DataTypes.DoubleType, false), DataTypes
-                            .createStructField("begin_time", DataTypes
-                                    .TimestampType, false), DataTypes
-                            .createStructField("last_time", DataTypes
-                                    .TimestampType, false), DataTypes
-                            .createStructField("distance", DataTypes
-                                    .IntegerType, false), DataTypes
-                            .createStructField("move_time", DataTypes
-                                    .IntegerType, false), DataTypes
-                            .createStructField("speed", DataTypes.DoubleType,
-                                    false), DataTypes.createStructField
-                            ("point_type", DataTypes.ByteType, false)));
-
-    public static final StructType OD_TRACE_SCHEMA = DataTypes
-            .createStructType(Lists.newArrayList(DataTypes.createStructField
-                    ("date", DataTypes.IntegerType, false), DataTypes
-                    .createStructField("msisdn", DataTypes.StringType, false)
-                    , DataTypes.createStructField("leave_base", DataTypes
-                            .StringType, false), DataTypes.createStructField
-                            ("leave_lng", DataTypes.DoubleType, false),
-                    DataTypes.createStructField("leave_lat", DataTypes
-                            .DoubleType, false), DataTypes.createStructField
-                            ("arrive_base", DataTypes.StringType, false),
-                    DataTypes.createStructField("arrive_lng", DataTypes
-                            .DoubleType, false), DataTypes.createStructField
-                            ("arrive_lat", DataTypes.DoubleType, false),
-                    DataTypes.createStructField("leave_time", DataTypes
-                            .TimestampType, false), DataTypes
-                            .createStructField("arrive_time", DataTypes
-                                    .TimestampType, false)));
-    public static final StructType OD_SCHEMA = DataTypes.createStructType
-            (Lists.newArrayList(DataTypes.createStructField("date", DataTypes
-                    .IntegerType, false), DataTypes.createStructField
-                    ("msisdn", DataTypes.StringType, false), DataTypes
-                    .createStructField("leave_base", DataTypes.StringType,
-                            false), DataTypes.createStructField("leave_lng",
-                    DataTypes.DoubleType, false), DataTypes.createStructField
-                    ("leave_lat", DataTypes.DoubleType, false), DataTypes
-                    .createStructField("arrive_base", DataTypes.StringType,
-                            false), DataTypes.createStructField("arrive_lng",
-                    DataTypes.DoubleType, false), DataTypes.createStructField
-                    ("arrive_lat", DataTypes.DoubleType, false), DataTypes
-                    .createStructField("leave_time", DataTypes.TimestampType,
-                            false), DataTypes.createStructField
-                    ("arrive_time", DataTypes.TimestampType, false),
-                    DataTypes.createStructField("linked_distance", DataTypes
-                            .IntegerType, false), DataTypes.createStructField
-                            ("max_speed", DataTypes.DoubleType, false),
-                    DataTypes.createStructField("cov_speed", DataTypes
-                            .DoubleType, false), DataTypes.createStructField
-                            ("distance", DataTypes.IntegerType, false),
-                    DataTypes.createStructField("move_time", DataTypes
-                            .IntegerType, false)));
-    public static final StructType OD_TRIP_STAT_SCHEMA = DataTypes
-            .createStructType(Lists.newArrayList(DataTypes.createStructField
-                    ("date", DataTypes.IntegerType, false), DataTypes
-                    .createStructField("msisdn", DataTypes.StringType, false)
                     , DataTypes.createStructField("has_trip", DataTypes
                             .ByteType, false), DataTypes.createStructField
                             ("staypoint_count", DataTypes.IntegerType, false)));
