1eb6c07d4cbae733d7365c91e5d621dbd9ebc726
chenhu <chenhu1008@gmail.com>
2020-08-31 21:15:26 +0800

    停留点路径

--- sky-root/signal-pre/src/main/java/com/sky/signal/pre/config/PathConfig.java
+++ sky-root/signal-pre/src/main/java/com/sky/signal/pre/config/PathConfig.java
@@ -35,6 +35,8 @@ public class PathConfig {
     public static final String OD_TRACE_SAVE_PATH = "od_trace/";
     //基础OD结果
     public static final String OD_SAVE_PATH = "od/";
+    //用于做区县od的停留点保存路径
+    public static final String OD_POINT_SAVE_PATH = "point/";
     //OD统计中间结果
     public static final String OD_STAT_TRIP_SAVE_PATH = "stat_trip/";
     //从全省原始信令中抽取在指定区县出现的手机号码的原始信令，保存原始信令的位置
